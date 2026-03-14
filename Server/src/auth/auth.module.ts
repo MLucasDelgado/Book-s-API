8f1f0c08229c5a1b083bbfb183f915f7df5892c2
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-03-14 12:25:32 -0300

    Configuracion del JWT

--- Server/src/auth/auth.module.ts
+++ Server/src/auth/auth.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { StringValue } from 'ms';
 
 @Module({
-  imports: [UsersModule], // Importa el módulo de usuarios para poder usar el servicio de usuarios en el servicio de autenticación.
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow<StringValue>('JWT_EXPIRES_IN'),
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
