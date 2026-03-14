102f080f7c35002fed0726b0c7569e307206a6f3
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-03-14 12:21:53 -0300

    Manejo de informacion para el controlador

---
+++ Server/src/auth/decorators/current-user.decorator.ts
@@ -0,0 +1,14 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { JwtPayload } from '../types/jwt-payload.type';
+import { Request } from 'express';
+
+interface RequestWithUser extends Request {
+  user: JwtPayload;
+}
+
+export const CurrentUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext): JwtPayload => {
+    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+    return request.user;
+  },
+);
