d3bf30fa8b71ac418e5cdcea58392800a4a54ee9
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-03-16 19:33:41 -0300

    Utilizacion del SetMetaData para las rutas

---
+++ Server/src/auth/decorators/roles.decorator.ts
@@ -0,0 +1,6 @@
+import { SetMetadata } from '@nestjs/common';
+import { Role } from '../enums/rol.enum';
+
+// ROLES_KEY es la clave que se utiliza para almacenar el rol en los metadatos de la ruta.
+export const ROLES_KEY = 'roles';
+export const Roles = (role: Role) => SetMetadata(ROLES_KEY, role);
