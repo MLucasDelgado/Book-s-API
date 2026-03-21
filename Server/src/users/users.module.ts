74acf040aa60528682319dd936740c5c1b3825d6
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-03-20 23:56:15 -0300

    Exportamos el userService

--- Server/src/users/users.module.ts
+++ Server/src/users/users.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])], // Importa el módulo de TypeORM para la entidad User.
   providers: [UsersService],
-  exports: [UsersService], // Exporta el servicio para que pueda ser utilizado en otros módulos, como AuthModule.
+  exports: [UsersService],
+  controllers: [UsersController], // Exporta el servicio para que pueda ser utilizado en otros módulos, como AuthModule.
 })
 export class UsersModule {}
