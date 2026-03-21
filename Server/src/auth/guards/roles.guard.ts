19484e9cbc2df7dc142e3c98f278be905f952686
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-03-20 23:44:58 -0300

    Rutas protegidas  por roles

--- Server/src/auth/guards/roles.guard.ts
+++ Server/src/auth/guards/roles.guard.ts
@@ -2,7 +2,7 @@ import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { RequestWithUser } from '../types/request-with-user.interface';
 import { ROLES_KEY } from '../decorators/roles.decorator';
-import { Role } from '../enums/rol.enum';
+import { Role } from '../../common/enums/rol.enum';
 
 @Injectable()
 // canActivate es el método que se ejecuta para determinar si el usuario tiene acceso a la ruta.
@@ -10,17 +10,23 @@ export class RolesGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const role = this.reflector.getAllAndOverride<Role>(ROLES_KEY, [
+    const roles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
 
-    if (!role) {
-      return true;
-    }
+    // Si no hay roles definidos, acceso libre
+    if (!roles) return true;
 
     const { user } = context.switchToHttp().getRequest<RequestWithUser>();
 
-    return role === user.role;
+    // Seguridad básica
+    if (!user) return false;
+
+    // ADMIN puede todo
+    if (user.role === Role.ADMIN) return true;
+
+    // USER u otros roles
+    return roles.includes(user.role);
   }
 }
