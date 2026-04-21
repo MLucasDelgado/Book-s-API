3d0865adced5068b2c69ed5946143d5dcbd5b127
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-04-20 22:54:21 -0300

    Importación de archivos con '@'

--- Server/src/books/books.module.ts
+++ Server/src/books/books.module.ts
@@ -3,11 +3,12 @@ import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
+import { AuthModule } from '@/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Book])],
+  imports: [AuthModule, TypeOrmModule.forFeature([Book])],
   providers: [BooksService],
   controllers: [BooksController],
-  exports: [BooksService], // Exportamos BooksService para que pueda ser utilizado en otros módulos, como FavoriteModule
+  exports: [BooksService], // Exportamos BooksService para que pueda ser utilizado en otros módulos
 })
 export class BooksModule {}
