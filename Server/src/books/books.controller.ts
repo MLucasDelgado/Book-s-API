a00669fdd65da1b6fd51e04319424fd58ad52a9c
MLucasDelgado <lucasdelgado1011@gmail.com>
2026-04-20 22:52:00 -0300

    Proteccion de ruta

--- Server/src/books/books.controller.ts
+++ Server/src/books/books.controller.ts
@@ -1,10 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { CreateBookDto } from '@/books/dto/book.dto';
+import { AuthGuard } from '@/auth/guards/auth.guard';
+import { Role } from '@/common/enums/rol.enum';
+import { Roles } from '@/auth/decorators/roles.decorator';
+import { RolesGuard } from '@/auth/guards/roles.guard';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
-  @Get('all')
+
+  @Roles(Role.USER)
+  @UseGuards(AuthGuard, RolesGuard)
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.create(createBookDto);
+  }
+
+  @Get()
   getBooks() {
     return this.booksService.getBooks();
   }
