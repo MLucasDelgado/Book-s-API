5070dedc1374339f06b9799ac4f3e791f914a225
Lucas Delgado <lucasdelgado1011@gmail.com>
2026-04-29 16:00:56 -0300

    Cambios en estructura del dto

--- Server/src/reviews/dto/create-review.dto.ts
+++ Server/src/reviews/dto/create-review.dto.ts
@@ -1,7 +1,6 @@
 import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
-import { CreateBookDto } from '@/books/dto/book.dto';
 
-export class CreateReviewDto extends CreateBookDto {
+export class CreateReviewDto {
   @IsInt()
   @Min(1)
   @Max(5)
